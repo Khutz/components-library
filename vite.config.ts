3c5e52a1b6eeb3c3ef4fdba96e9247530296987f
Oscar Estrada <oestrada@ia.com.mx>
2024-12-15 12:48:10 -0600

    config(vite): setup vite to lib mode

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,47 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
+import dts from "vite-plugin-dts";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    dts({
+      insertTypesEntry: true,
+      outDir: "dist",
+    }),
+  ],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/index.ts"),
+      name: "khutz-library",
+      formats: ["es", "umd"],
+      fileName: (format) => `khutz-library.${format}.js`,
+    },
+    rollupOptions: {
+      external: [
+        "react",
+        "react-dom",
+        "lucide-react",
+        "clsx",
+        "tailwind-merge",
+        "class-variance-authority",
+        "tailwindcss-animate",
+      ],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+          "lucide-react": "LucideReact",
+          clsx: "clsx",
+          "tailwind-merge": "tailwindMerge",
+          "class-variance-authority": "CVA",
+          "tailwindcss-animate": "tailwindcssAnimate",
+        },
+      },
+    },
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
