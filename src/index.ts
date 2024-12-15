2d1df5eaf8e154b8f9b2f05eb6ce604c867f4c7a
Oscar Estrada <oestrada@ia.com.mx>
2024-12-15 12:50:12 -0600

    feat(config): add entry point file

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+export { FooterBrand } from "@/components/footer-1/footer-brand";
+export { FooterLegal } from "@/components/footer-1/footer-legal";
+export { FooterLink } from "@/components/footer-1/footer-link";
+export { FooterLinkGroup } from "@/components/footer-1/footer-link-group";
+export { FooterNavigation } from "@/components/footer-1/footer-navigation";
+export { FooterSocialLinks } from "@/components/footer-1/footer-social-links";
+export { Button } from "@/components/ui/button";
+export { Input } from "@/components/ui/input";
